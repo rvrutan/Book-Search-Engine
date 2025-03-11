355f26fc83fe1a90efec4834efa3ffccd3f45edb
rvrutan <rutanroni@gmail.com>
2025-03-11 18:53:50 -0400

    vite.config updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,23 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ["library-6iwq.onrender.com"],
-    host: "0.0.0.0",
-    port: 3001,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+ import react from '@vitejs/plugin-react'
+ 
+ // https://vitejs.dev/config/
+ 
+ export default ({ mode }) => {
+   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+ 
+ return defineConfig({
+   plugins: [react()],
+   server: {
+     allowedHosts: ["library-6iwq.onrender.com"],
+     port: process.env.PORT || 3001,
+     open: true,
+     proxy: {
+       '/graphql': {
+         target: 'http://localhost:3001',
+         secure: false,
+         changeOrigin: true
+       }
+     }
+   }
+ })}
